aa2d3ea1ca0763d3b9188a6c84eec6f1e2413ceb
David Lucas <davidlucaskl@gmail.com>
2024-08-26 16:02:22 -0300

    feat : create models

---
+++ backend/models/Pet.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const PetSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    weight: {
+      type: Number,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: Array,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+    },
+    user: Object,
+    adopter: Object,
+  },
+  { timestamps: true }
+);
+
+const Pet = mongoose.model("Pet", PetSchema);
+export default Pet;
---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
